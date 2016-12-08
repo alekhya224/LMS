c469d8062e0695179d6a1db6518cb7a6b1f160a0
vikash-m <vikash.mahala@raremile.com>
2016-12-08 12:33:40 +0530

    Add Leave page

---
+++ EmployeeLeaveManagementWebAPI/DAL/Repositories/AddLeaveRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LMS_WebAPI_DAL.Repositories.Interfaces;
+using LMS_WebAPI_Domain;
+using LMS_WebAPI_DAL;
+
+namespace DAL.Repositories
+{
+    public class AddLeaveRepository 
+    {
+        public List<LeaveTypeModel> GetLeaveType()
+        {
+            using (var ctx = new LeaveManagementSystemEntities1())
+            {
+
+                var leavetypeid  = (from s in ctx.MasterDataTypes
+                                    where s.Type=="LeaveType"
+                                    select s).SingleOrDefault();
+
+                var leavetypes = (from s in ctx.MasterDataValues
+                                where s.RefMasterType == leavetypeid.Id
+                                select s).ToList();
+
+
+
+                if (leavetypes != null)
+                {
+                    return null;
+                }
+                else
+                    return null;
+            }
+        }
+    }
+    
+}
---
+++ EmployeeLeaveManagementWebAPI/EmployeeLeaveManagementWebAPI/Controllers/AddLeaveController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeLeaveManagementWebAPI.Controllers
+{
+    public class AddLeaveController : Controller
+    {
+        // GET: AddLeave
+        //public List<EmployeeLeaveTransactionModel> GetLeaveType()
+        //{
+        //    EmployeeLeaveTransactionManagement ELTM = new EmployeeLeaveTransactionManagement();
+        //    var res = ELTM.GetEmployeeLeaveTransaction();
+
+        //    return res;
+
+        //}
+    }
+}
