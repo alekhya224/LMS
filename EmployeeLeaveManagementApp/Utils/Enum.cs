a08be9c0bc17e68581c53f52142be496b25bfacb
DESKTOP-IEOF3HT\E7440 <alekhya224@users.noreply.github.com>
2016-12-09 21:53:12 +0530

    Add leave

---
+++ EmployeeLeaveManagementApp/Utils/Enum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS_WebAPP_Utils
+{
+ public enum LeaveType
+    {
+
+        [Description("Casual Leave")]
+        CasualLeave=8,
+        [Description("Sick Leave")]
+        SickLeave=7
+    }
+
+    public enum LeaveStatus
+    {
+
+        [Description("Added")]
+        Added = 9,
+        [Description("Submitted")]
+        Submitted = 10,
+        [Description("Rejected")]
+        Rejected =11,
+        [Description("Approved")]
+        Approved =12
+    }
+}
