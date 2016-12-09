aaff7648aab6388d01fea12529cb0ac31aff39a0
DESKTOP-IEOF3HT\E7440 <alekhya224@users.noreply.github.com>
2016-12-09 22:13:48 +0530

    add leave

---
+++ EmployeeLeaveManagementWebAPI/DAL/Repositories/Interfaces/IAddLeaveRepository.cs
@@ -0,0 +1,15 @@
+﻿using LMS_WebAPI_Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS_WebAPI_DAL.Repositories.Interfaces
+{
+  public  interface IAddLeaveRepository
+    {
+        List<string> GetLeaveType();
+        bool InsertEmployeeLeaveDetails(int leaveType, string fromDate, string toDate, string comments, int workingDays);
+    }
+}
