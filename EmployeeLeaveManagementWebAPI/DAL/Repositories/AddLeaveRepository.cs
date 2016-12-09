df4b75379f5c0738f520ca0ced5ee0d98c672ef0
DESKTOP-IEOF3HT\E7440 <alekhya224@users.noreply.github.com>
2016-12-09 22:48:12 +0530

    Add leave

--- EmployeeLeaveManagementWebAPI/DAL/Repositories/AddLeaveRepository.cs
+++ EmployeeLeaveManagementWebAPI/DAL/Repositories/AddLeaveRepository.cs
@@ -3,34 +3,65 @@ using System.Linq;
 using LMS_WebAPI_DAL.Repositories.Interfaces;
 using LMS_WebAPI_Domain;
 using LMS_WebAPI_DAL;
+using System;
 
-namespace DAL.Repositories
+namespace LMS_WebAPI_DAL.Repositories
 {
-    public class AddLeaveRepository 
+    public class AddLeaveRepository : IAddLeaveRepository
     {
-        public List<LeaveTypeModel> GetLeaveType()
+        public List<string> GetLeaveType()
         {
             using (var ctx = new LeaveManagementSystemEntities1())
             {
 
-                var leavetypeid  = (from s in ctx.MasterDataTypes
-                                    where s.Type=="LeaveType"
-                                    select s).SingleOrDefault();
+                //var leavetypeid  = (from s in ctx.MasterDataTypes
+                //                    where s.Type=="LeaveType"
+                //                    select s).SingleOrDefault();
 
-                var leavetypes = (from s in ctx.MasterDataValues
-                                where s.RefMasterType == leavetypeid.Id
-                                select s).ToList();
+                var leaveType = ctx.MasterDataValues.Where(x => x.RefMasterType == 3).Select(x => x.Value).ToList();
 
+                return leaveType;
 
-
-                if (leavetypes != null)
-                {
-                    return null;
-                }
-                else
-                    return null;
             }
         }
+
+        public bool InsertEmployeeLeaveDetails(int leaveType, string fromDate, string toDate, string comments, int workingDays)
+        {
+           
+            var result = false;
+
+            try
+            {
+                using (var ctx = new LeaveManagementSystemEntities1())
+                {
+
+                    var employeeLeaveDetails = new EmployeeLeaveTransaction
+                    {
+                        EmployeeComment = comments,
+                        FromDate = Convert.ToDateTime(fromDate),
+                        ToDate = Convert.ToDateTime(toDate),
+                        CreatedDate = DateTime.Now,
+                        NumberOfWorkingDays = workingDays,
+                        RefLeaveType = leaveType,
+                        RefStatus = 9,
+                        RefEmployeeId = 1,
+                        CreatedBy = "Alekya"
+                    };
+                    ctx.EmployeeLeaveTransactions.Add(employeeLeaveDetails);                       
+                        ctx.SaveChanges();
+                 
+
+                    }
+                    result = true;
+         
+            }
+            catch (Exception ex)
+            {
+                  throw;
+            }
+               return result;
+        }
+
     }
-    
+
 }
